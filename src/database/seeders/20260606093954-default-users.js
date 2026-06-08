6485f6ef1a4c81b9d5e67cc63abfaf1f541d0e3b
Er-AjayQA <official.ajay2681993@gmail.com>
2026-06-08 08:41:34 +0530

    added

--- src/database/seeders/20260606093954-default-users.js
+++ src/database/seeders/20260606093954-default-users.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const { v4: uuidv4 } = require("uuid");
-const bycrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
